161998977c7bebe705545f48821787c86fe2c34a
Christian Bikar <cbikar@liveperson.com>
2017-09-29 12:36:15 +0200

    First commit

---
+++ src/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" v-model="message"></input>
+    <button v-on:click="toggle">Toggle visibility</button>
+
+    <p v-if="visible">{{message}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: () => {
+    return {
+      message: "Hello World",
+      visible: true
+    }
+  },
+  methods: {
+    toggle() {
+      this.visible = !this.visible;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
