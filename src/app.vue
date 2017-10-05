892ce77468ac1f2db4713e288668e90970326330
Christian Bikar <cbikar@liveperson.com>
2017-10-05 16:31:29 +0200

    add example

--- src/app.vue
+++ src/app.vue
@@ -1,28 +1,44 @@
 <template>
-  <div>
-    <input type="text" v-model="message"></input>
-    <button v-on:click="toggle">Toggle visibility</button>
-
-    <p v-if="visible">{{message}}</p>
+  <div class="blog">
+    <div v-for="(post, index) in blogposts" v-bind:key="index">
+      <blogpost :id="index" :title="post.title" :text="post.text" v-on:remove="remove" />
+    </div>
   </div>
 </template>
 
 <script>
+import blogpost from './components/Blogpost.vue';
+
 export default {
   name: 'app',
+  components: { blogpost },
   data: () => {
     return {
-      message: "Hello World",
-      visible: true
+      blogposts: [
+        {
+          title: 'Intro to VueJS',
+          text: 'Lorem ipsum'
+        },
+        {
+          title: 'VueJS for Beginners',
+          text: 'Lorem ipsum'
+        }
+      ]
     }
   },
   methods: {
-    toggle() {
-      this.visible = !this.visible;
+    remove(id) {
+      this.blogposts.splice(id, 1);
     }
   }
 }
 </script>
 
 <style>
+
+.blog {
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+}
 </style>
---
+++ src/components/Blogpost.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="post">
+        <h1>{{title}}</h1>
+        <p>{{text}}</p>
+        <button v-on:click="remove">Remove</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'blogpost',
+    props: ['id', 'title', 'text'],
+    methods: {
+        remove() {
+            this.$emit('remove', this.id);
+        }
+    }
+}
+</script>
+
+<style>
+h1,
+p {
+  font-family: sans-serif
+}
+
+.post {
+    padding: 20px;
+    border: 1px solid;
+}
+
+button {
+    border: 0;
+}
+</style>
