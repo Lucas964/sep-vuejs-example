c2f321e16b442c88e67bc8966df543e330e36089
Christian Bikar <cbikar@liveperson.com>
2017-10-06 16:13:20 +0200

    added vue-resource example

--- src/app.vue
+++ src/app.vue
@@ -1,28 +1,43 @@
 <template>
   <div>
-    <input type="text" v-model="message"></input>
-    <button v-on:click="toggle">Toggle visibility</button>
-
-    <p v-if="visible">{{message}}</p>
+    <div class="episode" v-for="episode in episodes" v-bind:key="episode.id">
+      <h3>{{episode.name}}</h3>
+      <div>
+      <img v-if="episode.image" :src="episode.image.medium"></img>
+      <p class="summary" v-html="episode.summary" ></p> 
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  created () {
+    this.$http.get('http://api.tvmaze.com/shows/169/episodes').then(response => {
+      this.episodes = response.body;
+  }, response => {
+    console.log("error fetching data");
+  });
+  },
   data: () => {
     return {
-      message: "Hello World",
-      visible: true
-    }
-  },
-  methods: {
-    toggle() {
-      this.visible = !this.visible;
+      episodes: []
     }
   }
 }
 </script>
 
 <style>
+.episode {
+  padding: 20px;
+  clear: both;
+}
+
+img {
+  float: left;
+  padding-right: 10px;
+}
+
 </style>
