afd447ba53e71cc9069d60317c9981b481f3d0f6
Christian Bikar <cbikar@liveperson.com>
2017-10-16 16:25:23 +0200

    use fetch instead of vue resource

--- src/app.vue
+++ src/app.vue
@@ -3,23 +3,28 @@
     <div class="episode" v-for="episode in episodes" v-bind:key="episode.id">
       <h3>{{episode.name}}</h3>
       <div>
-      <img v-if="episode.image" :src="episode.image.medium"></img>
-      <p class="summary" v-html="episode.summary" ></p> 
+        <img v-if="episode.image" :src="episode.image.medium"></img>
+        <p class="summary" v-html="episode.summary"></p>
       </div>
     </div>
-    
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  created () {
-    this.$http.get('http://api.tvmaze.com/shows/169/episodes').then(response => {
-      this.episodes = response.body;
-  }, response => {
-    console.log("error fetching data");
-  });
+  created() {
+    fetch('http://api.tvmaze.com/shows/169/episodes')
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+        this.episodes = json;
+    })
+    .catch(err => {
+      console.log("Failed to fetch data", err);
+    });
   },
   data: () => {
     return {
@@ -39,5 +44,4 @@ img {
   float: left;
   padding-right: 10px;
 }
-
 </style>
